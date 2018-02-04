2894592d3e627e89c2351c526a8b1c7245a9a1ad
Gabriel Rolim <gabrielsrolim@gmail.com>
2018-02-04 12:40:13 -0300

    transformado Book em Functional Stateless Component.

--- src/Book.js
+++ src/Book.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 
-class Book extends Component {
 
-  changeOptionShelf = (event) => {
-    const { book,bookChangeShelf } = this.props
+const Book = ({book,bookChangeShelf}) => {
+
+  const changeOptionShelf = (event) => {
     bookChangeShelf(book,event.target.value)
   };
 
-  render () {
-    const { book } = this.props
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-          <div className="book-shelf-changer">
-            <select value={book.shelf == null ? "none" : book.shelf} onChange={ this.changeOptionShelf } >
-              <option value="disable" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+        <div className="book-shelf-changer">
+          <select value={book.shelf ? book.shelf : 'none'} onChange={ changeOptionShelf } >
+            <option value="disable" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+            <option value="none">None</option>
+          </select>
         </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors != null && book.authors.length === 1? book.authors[0]: book.authors != null && book.authors.map((b,index,book) => { return b + (index < book.length-1 ? ', ': '')})}</div>
       </div>
-    )
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.authors != null && book.authors.length === 1? book.authors[0]: book.authors != null && book.authors.map((b,index,book) => { return b + (index < book.length-1 ? ', ': '')})}</div>
+    </div>
+  )
+
 }
 
 export default Book
