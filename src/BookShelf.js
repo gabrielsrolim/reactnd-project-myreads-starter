f8c3467d27a4842390d92c91d778c5edae0909cf
Gabriel Rolim <gabrielsrolim@gmail.com>
2018-02-04 12:40:51 -0300

    transformado BookShelf em Functional Stateless Component.

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,39 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Book from './Book'
 
 
-class BookShelf extends Component {
+const BookShelf= ({ books, title, bookChangeShelf }) => {
 
-  getTitleShelf = (tipo) => {
-      if(tipo === 'currentlyReading'){
-        return 'Currently Reading'
-      } else if(tipo === 'wantToRead') {
-        return 'Want to Read'
-      } else if(tipo === 'read') {
-        return 'Read'
-      }
-  }
-
-  render () {
-    const { books, title, bookChangeShelf } = this.props
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-          {
-            books.map((b) =>
-              <li key={b.id}>
-                <Book book={b} bookChangeShelf={ bookChangeShelf }/>
-              </li>
-            )
-          }
-          </ol>
-        </div>
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+        {
+          books.map((b) =>
+            <li key={b.id}>
+              <Book book={b} bookChangeShelf={ bookChangeShelf }/>
+            </li>
+          )
+        }
+        </ol>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 
